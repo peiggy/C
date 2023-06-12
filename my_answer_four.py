8ffcfd6cdebd7b6a7b75027c035c214c5ad8e08a
peiggy carla <3099232503@qq.com>
2023-06-12 16:39:57 +0000

    save

--- my_answer_four.py
+++ my_answer_four.py
@@ -1,3 +1,7 @@
-#用lambda函数实现两个数相乘 
-sum=lambda a,b:a*b
-print(sum(5,4))
+#两个列表[1,5,7,9]和[2,2,6,8]合并为[1,2,2,3,6,7,8,9]
+list1=[1,5,7,9]
+list2=[2,2,6,8]
+list1.extend(list2)
+print(list1)
+list1.sort(reverse=False)
+print(list1)
