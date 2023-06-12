63e2f5f26e1ebcb4b62c5a5467417dec663d7153
peiggy carla <3099232503@qq.com>
2023-06-12 17:04:21 +0000

    save

--- my_answer_eight.py
+++ my_answer_eight.py
@@ -1,3 +1,9 @@
-#a = " hehheh ",去除收尾空格
-a=" hehheh "
-a.strip()
+#生成一个随机函数
+def fn():
+    try:
+        for i in range(5):
+            if i>2:
+                raise Exception("数字大于2了")
+    except Exception as ret:
+        print(ret)
+fn()
